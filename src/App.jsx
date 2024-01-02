9bbdaeebf2ffd82695431fc463f3c02510c5d219
TamannaTahsin1 <tamanntahsin648@gmail.com>
2024-01-02 13:22:56 +0600

    categories done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import './App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import OptionSelection from './Components/OptionSelection';
+import { arrayItems } from './AiOptions';
+
+
+
 
 function App() {
-
-
-
+console.log(arrayItems);
   return (
     <>
-
-    <OptionSelection></OptionSelection>
+    <OptionSelection arrayItems={arrayItems}></OptionSelection>
   
     </>
   )
--- src/Components/OptionSelection.jsx
+++ src/Components/OptionSelection.jsx
@@ -1,8 +1,25 @@
+/* eslint-disable react/prop-types */
 
-const OptionSelection = () => {
+const OptionSelection = ({arrayItems}) => {
     return (
-        <div>
-            <h1>React AI App</h1>
+        <div className="container mx-auto">
+            <h1 className="text-center font-bold text-4xl mb-20">React AI App</h1>
+
+           <div >
+            {
+                arrayItems?.map((item) => {
+                    return <>
+         
+                    <div className="border-4 mb-4 py-6 ">
+
+                    <h3 className="font-bold">{item.name}</h3>
+                    <p className="text-gray-500">{item.description}</p>
+                    </div>
+             
+                    </>
+                })
+            }
+           </div>
         </div>
     );
 };
